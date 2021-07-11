54e486521caee712dcc2d06fe64b8b59142b5f0c
Clay Chang <clay.chang@gmail.com>
2021-07-11 21:48:31 +0800

    Three main changes in this large commit:
    1) Add --vp-workspace for indicating virtplat platform worksace
    2) Use self-built edk2 ROM images
    3) Add host_share virtfs

--- fedora_rootfs.sh
+++ fedora_rootfs.sh
@@ -5,3 +5,6 @@
 mkdir -p "$1/etc/dnf/"
 dnf_conf=/etc/dnf/dnf.conf
 [ -f "$dnf_conf" ] && cp -L "$dnf_conf" "$1/$dnf_conf"
+
+printf "#!/bin/bash\\n\\nmkdir -p /root/host_share\\nmount -t 9p -o trans=virtio host_share /root/host_share -oversion=9p2000.L,posixacl,msize=104857600,cache=loose\\n" > "$1/root/mount_9p.sh"
+chmod +x "$1/root/mount_9p.sh"
